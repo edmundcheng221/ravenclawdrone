1c45802e4c866c365014dc8c7d949f5a6de4f47f
Edmund Cheng <ec3219@nyu.edu>
2021-02-24 18:57:52 -0500

    added navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        title: "Home",
+        url: "",
+        cName: "nav-links"
+    },
+    {
+        title: "About Us",
+        url: "",
+        cName: "nav-links"
+    },
+    {
+        title: "Our Product",
+        url: "",
+        cName: "nav-links"
+    },
+    {
+        title: "Contact Us",
+        url: "",
+        cName: "nav-links"
+    },
+
+]
